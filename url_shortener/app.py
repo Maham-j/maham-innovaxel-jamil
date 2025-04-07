983e18448201d7848cb13daeb296623c3e7e6a79
Maham.J <139694482+Maham-j@users.noreply.github.com>
2025-04-07 13:23:47 +0500

    Update app.py

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,1 +1,10 @@
-done
+from flask import Flask
+from database import create_app
+from routes import routes
+
+app = create_app()
+app.register_blueprint(routes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
